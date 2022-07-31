c85bd29baa908c59d6709f08b10e93d7a427043f
Rusiek <szymon.rusiecki@protonmail.com>
2022-07-31 17:26:49 +0200

    pycodestyle improvement

--- WDI/Zestaw_1/Zadanie_20/test_basic.py
+++ WDI/Zestaw_1/Zadanie_20/test_basic.py
@@ -4,7 +4,7 @@ from random import randint
 from ....readstdout import checkstdout
 from ....randTemplates.r_float import *
 from .prog import f as user_sol
-from .sol  import f as corr_sol
+from .sol import f as corr_sol
 
 basicTests = [
     (0, 0),
@@ -22,17 +22,18 @@ MIN_RANGE = 0
 MAX_RANGE = 1000
 TEST_NUM = 90
 basicTestsRandom = [
-    (randint(MIN_RANGE, MAX_RANGE), 
+    (randint(MIN_RANGE, MAX_RANGE),
      randint(MIN_RANGE, MAX_RANGE))
     for _ in range(TEST_NUM)]
 
+
 @pytest.mark.order(1)
-@pytest.mark.dependency(name = "testBasic", scope = "session")
+@pytest.mark.dependency(name="testBasic", scope="session")
 class TestBasic:
     @pytest.mark.parametrize("data", basicTests)
     def testBasic(self, data):
-        assert checkstdout(user_sol, corr_sol, data, float_type = True)
+        assert checkstdout(user_sol, corr_sol, data, float_type=True)
 
     @pytest.mark.parametrize("data", basicTestsRandom)
     def testBasicRandom(self, data):
-        assert checkstdout(user_sol, corr_sol, data, float_type = True)
+        assert checkstdout(user_sol, corr_sol, data, float_type=True)
