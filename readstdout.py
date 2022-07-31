49531310086120c3ae5171d73633b48e7310d669
Rusiek <szymon.rusiecki@protonmail.com>
2022-07-31 17:19:36 +0200

    pycodestyle improvement

--- readstdout.py
+++ readstdout.py
@@ -2,15 +2,18 @@ from io import StringIO
 from .randTemplates.r_float import *
 import sys
 
+
 class Capturing(list):
     def __enter__(self):
         self._stdout = sys.stdout
         sys.stdout = self._stringio = StringIO()
         return self
+
     def __exit__(self, *args):
         self.extend(self._stringio.getvalue().splitlines())
         del self._stringio
         sys.stdout = self._stdout
+
 
 def prepare(data, float_type):
     if float_type:
@@ -24,11 +27,14 @@ def prepare(data, float_type):
         output = "".join(data)
     return output[0]
 
-def checkstdout(f, g, arg, float_type = None):
+
+def checkstdout(f, g, arg, float_type=None):
     output_f, output_g = None, None
 
-    with Capturing() as output_f: f(*arg)
-    with Capturing() as output_g: g(*arg)
+    with Capturing() as output_f:
+        f(*arg)
+    with Capturing() as output_g:
+        g(*arg)
 
     output_f = prepare(output_f, float_type)
     output_g = prepare(output_g, float_type)
