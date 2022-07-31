f3281786d224c0af8e572ee4509615a18ab93b05
Rusiek <szymon.rusiecki@protonmail.com>
2022-07-31 17:28:43 +0200

    pycodestyle improvement

--- WDI/Zestaw_1/Zadanie_01/prog.py
+++ WDI/Zestaw_1/Zadanie_01/prog.py
@@ -1,9 +1,3 @@
-# Szymon Rusiecki
 def f():
-    a, b = 0, 1
-    print(a)
-    print(b)
-    while b < 10 ** 6:
-        a, b = b, a + b
-        print(b)
-    return
+    # Tutaj wprowadź swój kod
+    pass
