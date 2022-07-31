0460fbfb8c441e09bb70228e9ac60ac35772ce38
Rusiek <szymon.rusiecki@protonmail.com>
2022-07-31 17:28:23 +0200

    pycodestyle improvement

--- WDI/Zestaw_1/Zadanie_20/prog.py
+++ WDI/Zestaw_1/Zadanie_20/prog.py
@@ -1,9 +1,6 @@
-# Szymon Rusiecki
 from math import sqrt
 
+
 def f(a, b):
-    APPROX_ERROR = 10 ** (-6)
-    while (abs(a - b) > APPROX_ERROR):
-        a, b = sqrt(a * b), (a + b) / 2
-        
-    print((a + b) / 2)
+    # Tutaj wprowadź swój kod
+    pass
--- WDI/Zestaw_1/Zadanie_20/sol.py
+++ WDI/Zestaw_1/Zadanie_20/sol.py
@@ -1,9 +1,10 @@
 # Szymon Rusiecki
 from math import sqrt
 
+
 def f(a, b):
     APPROX_ERROR = 10 ** (-6)
-    while (a - b) > APPROX_ERROR or (b - a) > APPROX_ERROR :
+    while (a - b) > APPROX_ERROR or (b - a) > APPROX_ERROR:
         a, b = sqrt(a * b), (a + b) / 2
-        
+
     print((a + b) / 2)
